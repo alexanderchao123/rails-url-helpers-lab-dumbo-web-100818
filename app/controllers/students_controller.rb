d3e6427bb9dec11496a6e7b6b4bd6557e48172a0
github username <chao.alexander.123@gmail.com>
2018-11-03 17:38:28 -0400

    add activate route, activate action and activate view

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,17 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: [:show, :activate]
+
   def index
     @students = Student.all
   end
 
   def show
+    @student_active_message = set_student.active ? "This student is currently active." : "This student is currently inactive."
+  end
+
+  def activate
+    set_student.active ? set_student.update(active: false) : set_student.update(active: true)
+    redirect_to student_path(set_student)
   end
 
   private
